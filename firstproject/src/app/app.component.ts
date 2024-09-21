d24d4423ee0e670d92428a59e613cfdba19c3adc
Dennis Peter <peterdennis573@gmail.com>
2024-09-21 07:48:13 +0300

    Set up 3 components to learn angular bindings and routing navbar,footer,hero

--- firstproject/src/app/app.component.ts
+++ firstproject/src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+import { FooterComponent } from './components/footer/footer.component';
+
+import { HeroComponent } from './components/hero/hero.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavbarComponent,FooterComponent,HeroComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl:'./app.component.css',
 })
 export class AppComponent {
   title = 'firstproject';
