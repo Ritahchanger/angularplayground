08987c7b5fd26074e231ffca2330297b6474f810
Dennis Peter <peterdennis573@gmail.com>
2024-09-21 19:38:55 +0300

    Set up login page

---
+++ todolist/src/app/pages/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+import { FormsModule } from "@angular/forms"
+
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  
+  loginObj:any = {
+
+    "EmailId":"admin@gmail.com",
+    "Password":"12345678"
+
+  }
+
+
+}
